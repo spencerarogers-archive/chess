e0be448b01568b9a4a8485fc8961d5440ddafb0b
Spencer Rogers <spencer1248@gmail.com>
2016-01-24 05:25:37 -0500

    Get things cookin

--- test/chess_test.exs
+++ test/chess_test.exs
@@ -2,7 +2,10 @@ defmodule ChessTest do
   use ExUnit.Case
   doctest Chess
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "making a valid move" do
+    board = Chess.new_board
+    piece = Chess.piece_at(board, :a2)
+    Chess.move(board, :a2, :a3)
+    assert(Chess.piece_at(board, :a2) == piece)
   end
 end
