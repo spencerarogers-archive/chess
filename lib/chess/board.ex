b25d70e0329996eac24cb74545b8e64cb04b0a35
Spencer Rogers <spencer1248@gmail.com>
2016-01-27 12:02:31 -0500

    Simplify piece naming

--- lib/chess/board.ex
+++ lib/chess/board.ex
@@ -3,14 +3,14 @@ defmodule Chess.Board do
 
   def new do
     %{
-      h: %{ 1 => rook(black), 2 => horsey(black), 3 => bishop(black), 4 => queen(black), 5 => king(black), 6 => bishop(black), 7 => horsey(black), 8 => rook(black) },
-      g: %{ 1 => pawn(black), 2 => pawn(black),   3 => pawn(black),   4 => pawn(black),  5 => pawn(black), 6 => pawn(black),   7 => pawn(black),   8 => pawn(black) },
-      f: %{ 1 => nil,         2 => nil,           3 => nil,           4 => nil,          5 => nil,         6 => nil,           7 => nil,           8 => nil },
-      e: %{ 1 => nil,         2 => nil,           3 => nil,           4 => nil,          5 => nil,         6 => nil,           7 => nil,           8 => nil },
-      d: %{ 1 => nil,         2 => nil,           3 => nil,           4 => nil,          5 => nil,         6 => nil,           7 => nil,           8 => nil },
-      c: %{ 1 => nil,         2 => nil,           3 => nil,           4 => nil,          5 => nil,         6 => nil,           7 => nil,           8 => nil },
-      b: %{ 1 => pawn(white), 2 => pawn(white),   3 => pawn(white),   4 => pawn(white),  5 => pawn(white), 6 => pawn(white),   7 => pawn(white),   8 => pawn(white) },
-      a: %{ 1 => rook(white), 2 => horsey(white), 3 => bishop(white), 4 => queen(white), 5 => king(white), 6 => bishop(white), 7 => horsey(white), 8 => rook(white) }
+      h: %{1 => r(:b), 2 => k(:b), 3 => b(:b), 4 => q(:b), 5 => k(:b), 6 => b(:b), 7 => k(:b), 8 => r(:b)},
+      g: %{1 => p(:b), 2 => p(:b), 3 => p(:b), 4 => p(:b), 5 => p(:b), 6 => p(:b), 7 => p(:b), 8 => p(:b)},
+      f: %{1 => nil,   2 => nil,   3 => nil,   4 => nil,   5 => nil,   6 => nil,   7 => nil,   8 => nil  },
+      e: %{1 => nil,   2 => nil,   3 => nil,   4 => nil,   5 => nil,   6 => nil,   7 => nil,   8 => nil  },
+      d: %{1 => nil,   2 => nil,   3 => nil,   4 => nil,   5 => nil,   6 => nil,   7 => nil,   8 => nil  },
+      c: %{1 => nil,   2 => nil,   3 => nil,   4 => nil,   5 => nil,   6 => nil,   7 => nil,   8 => nil  },
+      b: %{1 => p(:w), 2 => p(:w), 3 => p(:w), 4 => p(:w), 5 => p(:w), 6 => p(:w), 7 => p(:w), 8 => p(:w)},
+      a: %{1 => r(:w), 2 => k(:w), 3 => b(:w), 4 => q(:w), 5 => k(:w), 6 => b(:w), 7 => k(:w), 8 => r(:w)}
     }
   end
 
--- lib/chess/piece.ex
+++ lib/chess/piece.ex
@@ -1,29 +1,13 @@
 defmodule Chess.Piece do
   defstruct name: nil, color: nil
 
-  @white :w
-  @black :b
-
-  def white do
-    @white
-  end
-
-  def black do
-    @black
-  end
+  @pieces [:p, :r, :k, :b, :q, :k]
 
   def valid_movements(board, {x1,y1}) do
     [{:c, 1}]
   end
 
-  [
-    :pawn,
-    :rook,
-    :horsey,
-    :bishop,
-    :queen,
-    :king
-  ] |> Enum.each fn name ->
+  @pieces |> Enum.each fn name ->
     def unquote(name)(color) do
       %Chess.Piece{ name: unquote(name), color: color }
     end
