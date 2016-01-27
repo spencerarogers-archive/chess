8043646784c408e885057c47706b48db1c55341d
Spencer Rogers <spencer1248@gmail.com>
2016-01-27 12:27:05 -0500

    Add character maps for pieces.

--- lib/chess/piece.ex
+++ lib/chess/piece.ex
@@ -1,7 +1,30 @@
 defmodule Chess.Piece do
   defstruct name: nil, color: nil
 
-  @pieces [:p, :r, :k, :b, :q, :k]
+  @pieces [:k, :q, :r, :b, :n, :p]
+  @pieces_to_utf8 %{
+    :k => %{ :w => '♔', :b => '♚' },
+    :q => %{ :w => '♕', :b => '♛' },
+    :r => %{ :w => '♖', :b => '♜' },
+    :b => %{ :w => '♗', :b => '♝' },
+    :n => %{ :w => '♘', :b => '♞' },
+    :p => %{ :w => '♙', :b => '♟' }
+  }
+
+  @utf8_to_pieces %{
+    '♔' => {:k, :w},
+    '♕' => {:q, :w},
+    '♖' => {:r, :w},
+    '♗' => {:b, :w},
+    '♘' => {:n, :w},
+    '♙' => {:p, :w},
+    '♚' => {:k, :b},
+    '♛' => {:q, :b},
+    '♜' => {:r, :b},
+    '♝' => {:b, :b},
+    '♞' => {:n, :b},
+    '♟' => {:p, :b}
+  }
 
   def valid_movements(board, {x1,y1}) do
     [{:c, 1}]
