2f86a83d8cc8b282d5ef0f45489b07f5ee62e16e
Spencer Rogers <spencer1248@gmail.com>
2016-02-16 22:30:45 -0500

    Pawn moves.

--- lib/chess/piece/pawn.ex
+++ lib/chess/piece/pawn.ex
@@ -1,29 +1,12 @@
 defmodule Chess.Piece.Pawn do
   def moves do
     [
-      fun({x, y}) -> {
-        :move, {x, y+1}}
-      end,
-      fun({x, y}) -> {
-        :home_row_move,
-        {x, y+2}}
-      end,
-      fun({x, y}) -> {
-        :capture,
-        {x+1, y+1}}
-      end,
-      fun({x, y}) -> {
-        :capture,
-        {x-1, y+1}}
-      end,
-      fun({x, y}) -> {
-        :en_passant,
-        {x-1, y+1}}
-      end,
-      fun({x, y}) -> {
-        :en_passant,
-        {x+1, y+1}}
-      end
+      fn({x,y}) -> {:move,       {x,   y+1}} end,
+      fn({x,y}) -> {:first_move, {x,   y+2}} end,
+      fn({x,y}) -> {:capture,    {x+1, y+1}} end,
+      fn({x,y}) -> {:capture,    {x-1, y+1}} end,
+      fn({x,y}) -> {:en_passant, {x-1, y+1}} end,
+      fn({x,y}) -> {:en_passant, {x+1, y+1}} end
     ]
   end
 end
