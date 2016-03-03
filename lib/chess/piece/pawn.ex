53c7e10fa41b2734b5c843e0934e52a6e0ccebb0
Spencer Rogers <spencer1248@gmail.com>
2016-03-02 21:33:50 -0500

    pawn 2 space opening

--- lib/chess/piece/pawn.ex
+++ lib/chess/piece/pawn.ex
@@ -18,7 +18,19 @@ defmodule Chess.Piece.Pawn do
             nil
         end
       end,
-      # fn({x,y}) -> {:first_move, {x,   y+(2*dir)}} end,
+      fn({x1,y1}) ->
+        space1 = {x1,y1+(1*dir)}
+        space2 = {x1,y1+(2*dir)}
+
+        valid = [space1, space2]
+        |> Enum.all?(fn(space) -> Chess.Board.unoccupied?(board, space) end)
+        |> case do
+          true ->
+            space2
+          false ->
+            nil
+        end
+      end,
       # fn({x,y}) -> {:capture,    {x+1, y+(1*dir)}} end,
       # fn({x,y}) -> {:capture,    {x-1, y+(1*dir)}} end,
       # fn({x,y}) -> {:en_passant, {x-1, y+(1*dir)}} end,
