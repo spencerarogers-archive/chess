bbcf62809f810fe47f6e63097579510df8651642
Spencer Rogers <spencer1248@gmail.com>
2016-03-13 23:35:26 -0400

    Grab correct piece module.

--- lib/chess/piece.ex
+++ lib/chess/piece.ex
@@ -26,6 +26,15 @@ defmodule Chess.Piece do
     "♟" => {:p, :black}
   }
 
+  @piece_modules %{
+    :k => Chess.Piece.King,
+    :q => Chess.Piece.Queen,
+    :r => Chess.Piece.Rook,
+    :b => Chess.Piece.Bishop,
+    :n => Chess.Piece.Knight,
+    :p => Chess.Piece.Pawn
+  }
+
   def piece_to_utf8(piece) do
     case piece do
       nil ->
@@ -59,8 +68,9 @@ defmodule Chess.Piece do
 
   def valid_movements(board, {x1,y1}) do
     %Chess.Piece{name: piece_type, color: color} = board |> Chess.Board.piece_at({x1,y1})
+    piece_module = Map.get(@piece_modules, piece_type)
 
-    Chess.Piece.Pawn.move_definitions(board, color)
+    piece_module.move_definitions(board, color)
     |> Enum.reduce(MapSet.new, fn (move_func, set) ->
       move_func.({x1,y1})
       |> case do
